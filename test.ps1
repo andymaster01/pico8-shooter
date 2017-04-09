2108e0b6e186eae8e57e00c8ebaa88be3ec3e458
mauro <mauro@mauro>
2017-04-08 23:38:31 -0400

    Update tasks to support run and build

--- test.ps1
+++ test.ps1
@@ -1,7 +1,16 @@
 $p8toolPath = ".\..\..\picotool\p8tool"
+$pico8Path = ".\..\..\apps\pico-8\pico8.exe"
 
-# Write-Output $args[1]
+$type = $args[1]
 
-Write-Output "test"
+if($type -eq "build"){
+    Write-Output "is build"
+}
+elseif($type -eq "run"){
+    Write-Output "is run"
+}
+else {
+    Write-Output "unknown"
+}
 
 # python $p8toolPath stats shooter.p8
