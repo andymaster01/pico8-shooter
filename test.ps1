56d707581c40bc9290edc74479d41eebe8f56905
mauro <mauro@mauro>
2017-04-08 23:45:21 -0400

    Add real calls to programs to build and run

--- test.ps1
+++ test.ps1
@@ -1,16 +1,16 @@
 $p8toolPath = ".\..\..\picotool\p8tool"
-$pico8Path = ".\..\..\apps\pico-8\pico8.exe"
+$pico8Path = "d:\apps\pico-8\pico8.exe"
 
 $type = $args[1]
 
 if($type -eq "build"){
     Write-Output "is build"
+    python $p8toolPath "stats" "shooter.p8"
 }
 elseif($type -eq "run"){
     Write-Output "is run"
+    &$pico8Path "-run" ".\shooter.p8"
 }
 else {
     Write-Output "unknown"
 }
-
-# python $p8toolPath stats shooter.p8
