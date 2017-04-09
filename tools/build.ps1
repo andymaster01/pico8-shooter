2b3ed24a045f51f4929ae25a8c82967b27e3c360
mauro <mauro@mauro>
2017-04-09 00:57:49 -0400

    Test version

--- tools/build.ps1
+++ tools/build.ps1
@@ -1,19 +1,25 @@
 $p8toolPath = ".\..\..\picotool\p8tool"
 $pico8Path = "d:\apps\pico-8\pico8.exe"
 
-$inputFile = "shooter.lua"
-$cartFile = "shooter.p8"
+
+$inputFile = "code\shooter.lua"
+$cartFile = "pico8-home\shooter.p8"
+
+$picoHome = "$($PSScriptRoot)\..\pico8-home"
+
 
 $type = $args[1]
 
 if($type -eq "build"){
+    Write-Output $PSScriptRoot
     Write-Output "Building..."
     python $p8toolPath "build" $cartFile "--lua" $inputFile
     Write-Output "Done with $($cartFile)"
 }
 elseif($type -eq "run"){
     Write-Output "Running $($cartFile) ..."
-    &$pico8Path "-run" $cartFile
+    Write-Output "Home is $($picoHome)"
+    &$pico8Path "-run" $cartFile "-home" $picoHome
 }
 else {
     Write-Output "unknown"
