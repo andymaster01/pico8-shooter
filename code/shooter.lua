211f418b38a54a505c9c89360eaf29eb36c26f07
mauro <mauro@mauro>
2017-04-09 02:07:44 -0400

    Add enemies collision

--- code/shooter.lua
+++ code/shooter.lua
@@ -1,26 +1,44 @@
 function _init()
     t = 0
 
-    ship = {
-        sp = 1,
-        x = 60,
-        y = 60,
-        h = 3
-    }
-
+    ship = { sp = 1, x = 60, y = 100, h = 3, p = 0,
+			 box = {x1 = 0, y1 = 0, x2 = 7, y2 = 7} 
+		   }
     bullets = {}
     enemies = {}
 
-    for i = 1, 10 do
+    for i = 1, 4 do
         add(enemies, {
             sp = 3,
-            m_x = i * 16,
-            m_y = 60 - i * 8,
-            x = -32,
-            y = -32,
-            r = 10
+			r = 12,
+            m_x = i * 16, m_y = 60 - i * 8,
+            x = -32,  y = -32,
+			box = {x1 = 0, y1 = 0, x2 = 7, y2 = 7} 
   	    })
     end
+end
+
+function abs_box(s)
+	local box = {}
+	box.x1 = s.box.x1 + s.x
+	box.y1 = s.box.y1 + s.y
+	box.x2 = s.box.x2 + s.x
+	box.y2 = s.box.y2 + s.y
+	return box
+end
+
+function coll(a,b)
+	local box_a = abs_box(a)
+	local box_b = abs_box(b)
+
+	if 	box_a.x1 > box_b.x2 or
+		box_a.y1 > box_b.y2 or
+		box_b.x1 > box_a.x2 or
+		box_b.y1 > box_a.y2 then
+		return false
+	end
+
+	return true
 end
 
 function fire()
@@ -29,7 +47,8 @@ function fire()
 		x = ship.x,
 		y = ship.y,
 		dx = 0,
-		dy = -3
+		dy = -3,
+		box = {x1 = 2, y1 = 0, x2 = 5, y2 = 4} 
 	}
 	add(bullets, b)
 end
@@ -49,7 +68,17 @@ function _update()
 		   b.y < 0 or b.y > 128 then
 		   del(bullets, b)
 		end
+
+		for e in all(enemies) do
+			if coll(b, e) then
+				del(enemies, e)
+				ship.p += 1
+			end
+		end
+
 	end
+
+	
 	
 	if (t%6<4) then
 		ship.sp = 0
@@ -66,7 +95,7 @@ end
 
 function _draw()
 	cls()
-	print(#bullets)
+	print(ship.p, 9)
 	spr(ship.sp, ship.x, ship.y)
 	
 	for b in all(bullets) do
