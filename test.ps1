175cbef68a5a473c174a1273d1d24e302efa99a9
mauro <mauro@mauro>
2017-04-08 23:46:29 -0400

    Use variable name for p8 file

--- test.ps1
+++ test.ps1
@@ -1,15 +1,17 @@
 $p8toolPath = ".\..\..\picotool\p8tool"
 $pico8Path = "d:\apps\pico-8\pico8.exe"
 
+$p8File = "shooter.p8"
+
 $type = $args[1]
 
 if($type -eq "build"){
     Write-Output "is build"
-    python $p8toolPath "stats" "shooter.p8"
+    python $p8toolPath "stats" $p8File
 }
 elseif($type -eq "run"){
     Write-Output "is run"
-    &$pico8Path "-run" ".\shooter.p8"
+    &$pico8Path "-run" $p8File
 }
 else {
     Write-Output "unknown"
